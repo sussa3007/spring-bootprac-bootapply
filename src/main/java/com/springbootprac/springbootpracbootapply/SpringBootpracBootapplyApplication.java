5eabb15400c41dfff496e6a70d718db2a77a6f24
sussa3007 <sussa1933@gmail.com>
2022-10-09 17:56:53 +0900

    Initialize #1- Spring Boot

---
+++ src/main/java/com/springbootprac/springbootpracbootapply/SpringBootpracBootapplyApplication.java
@@ -0,0 +1,13 @@
+package com.springbootprac.springbootpracbootapply;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootpracBootapplyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootpracBootapplyApplication.class, args);
+    }
+
+}
